e3145571ff0c5446bb83bbe2e425a33e9d7e0d44
김영훈 <eth220421@gmail.com>
2024-09-07 23:02:06 +0900

    refactor
    refactor :
     - withCredentials: true 추가

--- src/services/api/user/GetUserAPI.tsx
+++ src/services/api/user/GetUserAPI.tsx
@@ -2,7 +2,9 @@ import { axios } from "../../utils/axios";
 
 export const GetUserAPI = async () => {
   try {
-    const response = await axios.get(`/api/v1/user/getUser`);
+    const response = await axios.get(`/api/v1/user/getUser`, {
+      withCredentials: true,
+    });
     console.log("회원정보 조회 성공", response.data);
     return response.data;
   } catch (error) {
