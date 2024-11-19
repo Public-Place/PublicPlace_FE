2dcde2d4278bc9e2dc9c5acceb1788f4f03f9a88
김영훈 <eth220421@gmail.com>
2024-11-19 19:06:09 +0900

    refactor
    refactor :
     - BASE_URL을 .env에 작성 후 .gitignore에 추가

--- src/services/utils/axios.tsx
+++ src/services/utils/axios.tsx
@@ -1,26 +1,25 @@
 import Axios from "axios";
 
-const BASE_URL = "http://3.36.221.111:8080";
+// 환경 변수에서 BASE_URL 가져오기
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-// Axios 인스턴스 생성 (재사용성 ↑, 코드 간결성 ↑)
+if (!BASE_URL) {
+  throw new Error("BASE_URL 환경 변수가 설정되지 않았습니다.");
+}
+
+// Axios 인스턴스 생성
 export const axios = Axios.create({
   baseURL: BASE_URL,
 });
 
-// 요청 인터셉터 추가 (Request Interceptor)
+// 요청 인터셉터 추가
 axios.interceptors.request.use(
   (config) => {
-    // localStorage에서 JWT 토큰을 가져오기
     const token = localStorage.getItem("token");
-
-    // 토큰이 있을 경우 헤더에 추가
     if (token) {
       config.headers["X-AUTH-TOKEN"] = token;
     }
-
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
